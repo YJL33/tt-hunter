8d1bfc3a5ac85def757573e0f6e740f3f6f95f80
YJL33 <yunjun.l33@gmail.com>
2021-07-20 18:59:53 +0800

    Add youtube_flutter_player and video_list.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:addict_wall/main.dart';
+import 'package:addict_wall/maindart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
